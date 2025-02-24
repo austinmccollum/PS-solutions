9f1069b298d67483b72883a50223839bd31700c1
austinmccollum <austinmc@microsoft.com>
2025-02-24 12:09:45 -0600

    add today variable for uniqueness and visibility

--- New-SelfSignedApiCert.ps1
+++ New-SelfSignedApiCert.ps1
@@ -1,11 +1,15 @@
 # Make private key nonexportable - locks down access to your machine and login
 # Secure tenant restricts credential lifetime policy with max 30 days, so we create cert validity of 30 days
-New-SelfSignedCertificate -CertStoreLocation "cert:\CurrentUser\My" -Subject "My API testing cert" -NotAfter (Get-Date).AddDays(30) -KeySpec KeyExchange -KeyExportPolicy NonExportable -KeyUsage KeyEncipherment -KeyProtection None
 
-$certs = Get-ChildItem -Path cert:\CurrentUser\My | Where-Object { $_.Subject -eq "CN=My API testing cert" }
+# using date to make cert more unique and stand out in the list of certs
+$today = (Get-Date).ToString("yyyy-MM-dd")
+
+New-SelfSignedCertificate -CertStoreLocation "cert:\CurrentUser\My" -Subject "My API testing cert $today" -NotAfter (Get-Date).AddDays(30) -KeySpec KeyExchange -KeyExportPolicy NonExportable -KeyUsage KeyEncipherment -KeyProtection None
+
+$certs = Get-ChildItem -Path cert:\CurrentUser\My | Where-Object { $_.Subject -eq "CN=My API testing cert $today" }
 
 # This is the public key portion which we'll upload to the app registration
-Export-Certificate -Cert $certs.PSPath -FilePath ~\desktop\SelfSignedApiTestingCert.cer
+Export-Certificate -Cert $certs.PSPath -FilePath "~\desktop\SelfSignedApiTestingCert$($today).cer"
 
 # Here's the thumbprint which you'll need to get your auth token to call your API
 $certs.Thumbprint
